1424edb7dc147b7fab53af8e0eb6a5739506e700
Dmytro Shvechikov <quitereality@gmail.com>
2025-11-09 14:34:54 +0100

    fix test

--- src/test/java/fyi/dslab/children/budget/FamilyBudgetApplicationTests.java
+++ src/test/java/fyi/dslab/children/budget/FamilyBudgetApplicationTests.java
@@ -2,8 +2,12 @@ package fyi.dslab.children.budget;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class FamilyBudgetApplicationTests {
 
 	@Test
